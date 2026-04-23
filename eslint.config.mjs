dca71765dba16f5aedd755f93285e8ab2902c11c
Rakshit <rakshit.chd43@gmail.com>
2026-04-23 11:02:45 +0530

    test

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+import js from "@eslint/js";
+import globals from "globals";
+
+export default [
+  js.configs.recommended,
+
+  {
+    files: ["**/*.js"],
+
+    languageOptions: {
+      sourceType: "module",
+
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.jest
+      }
+    }
+  }
+];
